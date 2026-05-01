3b3ffb1b2ac8a09a01f17b14b133deecf28cd9e5
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-05-01 18:52:55 +0200

    feat(layout): event type presentation part

--- pages/home.php
+++ pages/home.php
@@ -8,7 +8,7 @@
       </h1>
     </div>
   </div>
-  <article>
+  <article id="welcome-info-part">
     <div class="container py-5">
       <div class="row align-items-center">
 
@@ -17,7 +17,7 @@
         </div>
 
         <div class="col-12 col-md-6 order-2 order-md-2">
-          <img class="img-fluid rounded" src="<?= BASE_URL ?>/assets/pictures/restaurant-v-et-g.jpg" />
+          <img class="img-same rounded" src="<?= BASE_URL ?>/assets/pictures/restaurant-v-et-g.jpg" />
         </div>
 
         <div class="col-12 col-md-6 order-3 order-md-1 layout-sm">
@@ -29,6 +29,35 @@
             grâce à des menus variés et en constante évolution.
           </p>
           
+          <a href="<?= BASE_URL ?>/" class="btn btn-primary button">Réserver</a>
+        </div>
+      </div>
+    </div>
+  </article>
+
+
+  <article class="bg-light bg-opacity-50" id="type-event-part">
+    <div class="container py-5">
+      <div class="row align-items-center">
+
+        <div class="col-12 text-center article-type-event m-0">
+          <h1 class="text-primary m-0">Traiteurs particuliers & professionnels</h1>
+          <h5 class="text-primary mb-5">De l'évenement au quotidien</h5>
+        </div>
+
+        <div class="row mid-pic ">
+          <div class="col-12 col-md-6 order-2 order-md-2 ps-0">
+            <img class="img-same rounded" src="<?= BASE_URL ?>/assets/pictures/private-meal.jpg" />
+            <p class="text-center">Préstation privées</p>
+          </div>
+
+          <div class="col-12 col-md-6 order-2 order-md-2 pe-0">
+            <img class="img-same rounded" src="<?= BASE_URL ?>/assets/pictures/event-meal.jpg" />
+            <p class="text-center">Préstations événementielles</p>
+          </div>
+        </div>
+
+        <div class="col-12 button-type-event-layout">          
           <a href="<?= BASE_URL ?>/" class="btn btn-primary button">Réserver</a>
         </div>
       </div>
