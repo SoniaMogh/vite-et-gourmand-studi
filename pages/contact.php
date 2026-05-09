6bfe3552f41c8d6bf0daae7b2b25f6daaad23021
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-05-09 18:55:04 +0200

    feat(layout): add contact page

---
+++ pages/contact.php
@@ -0,0 +1,50 @@
+<div id="contact-part">
+  <div class="container py-5">
+    <div class="row card mx-5 mx-md-6">
+      <div class="col-12 text-center">
+        <img
+          src="<?= BASE_URL ?>/assets/pictures/logo-primary.png"
+          alt="logo-v-et-g"
+          class="form-logo-size"
+        />
+        <h1 class="text-primary">Contactez-nous</h1>
+      </div>
+      <form action="<?= BASE_URL ?>/messageSent" method="post" class="form-display">
+        <div class="col-10">
+          <label for="reason-contact">Raison de la prise de contact</label>
+          <input
+            class="w-100 form-control"
+            type="text"
+            id="reason-contact"
+            name="reason-contact"
+            placeholder="Ex: Menu version végétarienne"
+            required
+          />
+        </div>
+        <div class="col-10">
+          <label for="description-contact">Description</label>
+          <textarea
+            class="w-100 form-control"
+            id="description-contact"
+            name="description-contact"
+            rows="3"
+            placeholder="Décrivez votre demande..."
+            required
+          ></textarea>
+        </div>
+        <div class="col-10">
+          <label for="email-contact">Email</label>
+          <input
+            class="w-100 form-control"
+            type="email"
+            id="email-contact"
+            name="email-contact"
+            placeholder="mail@hotmail.fr"
+            required
+          />
+        </div>
+        <input type="submit" value="Envoyer" class="btn btn-primary button">
+      </form>
+    </div>
+  </div>
+</div>
---
+++ pages/messageSent.php
@@ -0,0 +1,5 @@
+<div id="messageSent-page">
+    <h1>Votre message a bien été envoyé</h1>
+    <h3 class="text-primary">Vous receverez une reponse dans les 48h, par mail.</h3>
+    <a href="<?= BASE_URL ?>/">Retourner à l'accueil</a>
+</div>
