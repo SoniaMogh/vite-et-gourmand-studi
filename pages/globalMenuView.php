942e86cfed0d4380847556ae54e2ebe475f2b041
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-05-10 23:29:45 +0200

    feat(layout): detailed menu popup

--- pages/globalMenuView.php
+++ pages/globalMenuView.php
@@ -31,15 +31,12 @@
   <div id="menus">
     <div class="container pb-5 px-5">
       <div class="menu-card-wrapper">
-
         <!-- ---------------------------CARD 1--------------------------------- -->
-        <div class="card card-corner global-view-card p-0 bg-white">
-          <div class="img-menu-card">
-            <img
-              class="forced-in-div shadow"
-              src="<?= BASE_URL ?>/assets/pictures/fresh-product-key-point.jpg"
-            />
-          </div>
+        <div class="card card-corner p-0 bg-white">
+          <img
+            class="forced-in-div shadow"
+            src="<?= BASE_URL ?>/assets/pictures/fresh-product-key-point.jpg"
+          />
 
           <div class="p-4 display-info-menu">
             <div class="menu-info-text">
@@ -52,19 +49,24 @@
 
             <div class="menu-info-price">
               <p class="mb-0 mt-2">45 €/pers</p>
-              <a href="" class="btn btn-primary">Détails</a>
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#menuModal"
+              >
+                Détails
+              </button>
             </div>
           </div>
         </div>
 
         <!-- ---------------------------CARD 2--------------------------------- -->
-        <div class="card card-corner global-view-card p-0 bg-white">
-          <div class="img-menu-card">
-            <img
-              class="forced-in-div shadow"
-              src="<?= BASE_URL ?>/assets/pictures/fresh-product-key-point.jpg"
-            />
-          </div>
+        <div class="card card-corner p-0 bg-white">
+          <img
+            class="forced-in-div shadow"
+            src="<?= BASE_URL ?>/assets/pictures/fresh-product-key-point.jpg"
+          />
 
           <div class="p-4 display-info-menu">
             <div class="menu-info-text">
@@ -77,7 +79,107 @@
 
             <div class="menu-info-price">
               <p class="mb-0 mt-2">45 €/pers</p>
-              <a href="" class="btn btn-primary">Détails</a>
+              <button
+                type="button"
+                class="btn btn-primary"
+                data-bs-toggle="modal"
+                data-bs-target="#menuModal"
+              >
+                Détails
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="menuModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="row modal-dialog modal-xl modal-dialog-centered">
+      <button type="button" class="btn-close text-end m-0" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div class="modal-content">
+        <div class="modal-body p-3 p-sm-5">
+          <h1 class="text-center">Conviviale & Gourmand</h1>
+
+          <div class="detailed-menu-card-wrapper">
+            <div class="card card-corner p-0 bg-white">
+              <img
+                class="forced-in-div shadow"
+                src="<?= BASE_URL ?>/assets/pictures/fresh-product-key-point.jpg"
+              />
+
+              <div class="p-4">
+                <div class="menu-info-text">
+                  <p>Partagez un repas en famille ou entre amis.</p>
+                  <p class="text-primary fw-bold m-0">
+                    Réservation 8 jours avant la date.
+                  </p>
+                  <p class="text-primary">2 personnes minimum</p>
+                  <p class="m-0 fst-italic text-primary test">
+                    Il reste 9 commandes possibles de ce menu
+                  </p>
+                  <p class="mb-0 mt-2 text-center">45 €/pers</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div class="card card-corner p-0 bg-white mb-4">
+                <div class="p-4 py-5">
+                  <div class="menu-info-text">
+                    <div id="entrée">
+                      <h4 class="text-center text-primary fw-bold">Entrée</h4>
+                      <h4 class="text-dark fw-bold mb-2">Bourrata Gourmande</h4>
+                      <p class="lh-1 mb-0 ms-4">
+                        Burrata crémeuse, tomates anciennes, pesto basilic,
+                        roquette, pignons torréfiés
+                      </p>
+                      <p class="text-primary mb-0 ms-4 lh-1">
+                        <i class="bi bi-info-circle text-primary"></i>
+                        Allergènes: Lait, fruits à coque (pignons)
+                      </p>
+                    </div>
+                    <div id="Plat-principal" class="mt-4">
+                      <h4 class="text-center text-primary fw-bold">Entrée</h4>
+                      <h4 class="text-dark fw-bold mb-2">Bourrata Gourmande</h4>
+                      <p class="lh-1 mb-0 ms-4">
+                        Burrata crémeuse, tomates anciennes, pesto basilic,
+                        roquette, pignons torréfiés
+                      </p>
+                      <p class="text-primary mb-0 ms-4 lh-1">
+                        <i class="bi bi-info-circle text-primary"></i>
+                        Allergènes: Lait, fruits à coque (pignons)
+                      </p>
+                    </div>
+                    <div id="Dessert" class="mt-4">
+                      <h4 class="text-center text-primary fw-bold">Entrée</h4>
+                      <h4 class="text-dark fw-bold mb-2">Bourrata Gourmande</h4>
+                      <p class="lh-1 mb-0 ms-4">
+                        Burrata crémeuse, tomates anciennes, pesto basilic,
+                        roquette, pignons torréfiés
+                      </p>
+                      <p class="text-primary mb-0 ms-4 lh-1">
+                        <i class="bi bi-info-circle text-primary"></i>
+                        Allergènes: Lait, fruits à coque (pignons)
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="row justify-content-center">
+              <button
+                type="button"
+                class="btn btn-primary "
+              >
+                Commander
+              </button>
+              </div>
             </div>
           </div>
         </div>
