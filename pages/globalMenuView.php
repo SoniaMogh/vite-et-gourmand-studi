bda8c1ba70b17cdf1862a9fa324480f27ba88f01
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-05-10 16:53:55 +0200

    feat(layout): cards display

--- pages/globalMenuView.php
+++ pages/globalMenuView.php
@@ -6,7 +6,7 @@
   </div>
 
   <div id="global-view-filter">
-    <div class="container py-5 px-0">
+    <div class="container py-5 px-5">
       <div class="bg-white card-corner p-4">
         <div class="col-2 form-display">
           <label for="theme">Theme</label>
@@ -29,9 +29,11 @@
   </div>
 
   <div id="menus">
-    <div class="container py-5">
-      <div class="row align-items-start">
-        <div class="bg-white card card-corner global-view-card p-0">
+    <div class="container pb-5 px-5">
+      <div class="menu-card-wrapper">
+
+        <!-- ---------------------------CARD 1--------------------------------- -->
+        <div class="card card-corner global-view-card p-0 bg-white">
           <div class="img-menu-card">
             <img
               class="forced-in-div shadow"
@@ -40,7 +42,7 @@
           </div>
 
           <div class="p-4 display-info-menu">
-            <div class="col-9">
+            <div class="menu-info-text">
               <h2 class="m-0">Esprit de fête</h2>
               <p class="text-primary">2 personnes minimum</p>
               <p class="m-0">
@@ -48,7 +50,32 @@
               </p>
             </div>
 
-            <div class="col-3 text-left">
+            <div class="menu-info-price">
+              <p class="mb-0 mt-2">45 €/pers</p>
+              <a href="" class="btn btn-primary">Détails</a>
+            </div>
+          </div>
+        </div>
+
+        <!-- ---------------------------CARD 2--------------------------------- -->
+        <div class="card card-corner global-view-card p-0 bg-white">
+          <div class="img-menu-card">
+            <img
+              class="forced-in-div shadow"
+              src="<?= BASE_URL ?>/assets/pictures/fresh-product-key-point.jpg"
+            />
+          </div>
+
+          <div class="p-4 display-info-menu">
+            <div class="menu-info-text">
+              <h2 class="m-0">Esprit de fête</h2>
+              <p class="text-primary">2 personnes minimum</p>
+              <p class="m-0">
+                Partagez un plat chaud entouré de vos être chers pour Noël
+              </p>
+            </div>
+
+            <div class="menu-info-price">
               <p class="mb-0 mt-2">45 €/pers</p>
               <a href="" class="btn btn-primary">Détails</a>
             </div>
