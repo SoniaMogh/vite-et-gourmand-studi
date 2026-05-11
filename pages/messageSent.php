e7b9f47b15b87cb1ba6890894fe0f29b44d282b8
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-05-11 22:36:37 +0200

    feat(db): add mysql database connection

--- pages/messageSent.php
+++ pages/messageSent.php
@@ -1,5 +1,34 @@
+<?php
+require __DIR__ . "/../config/database.php";
+
+try {
+  $reasonContact= $_POST['reason-contact'];
+  $descriptionContact= $_POST['description-contact'];
+  $emailContact= $_POST['email-contact'];
+
+  $saveMessage = "INSERT INTO messages (email, title, description) VALUES (:email_contact, :reason_contact, :description_contact)";
+  $stmt = $pdo->prepare($saveMessage);
+  $stmt->bindParam(':email_contact', $emailContact);
+  $stmt->bindParam(':reason_contact', $reasonContact);
+  $stmt->bindParam(':description_contact', $descriptionContact);
+  $stmt->execute();
+?>
+
 <div id="messageSent-page">
-    <h1>Votre message a bien été envoyé</h1>
-    <h3 class="text-primary">Vous receverez une reponse dans les 48h, par mail.</h3>
-    <a href="<?= BASE_URL ?>/">Retourner à l'accueil</a>
+  <h1>Votre message a bien été envoyé</h1>
+  <h3 class="text-primary">Vous receverez une reponse dans les 48h, par mail.</h3>
+  <a href="<?= BASE_URL ?>/">Retourner à l'accueil</a>
 </div>
+
+<?php 
+  } catch(PDOException $e) { 
+?>
+
+<div id="messageSent-page">
+  <h1 class="text-warning">Une erreur est survenue</h1>
+  <h3 class="text-warning">Votre message n'a pas pu être envoyé. Veuillez rééssayer ulterieurement</h3>
+  <a href="<?= BASE_URL ?>/">Retourner à l'accueil</a>
+</div>
+
+<?php } ?>
+
