ee3bca687be191a6654c7348c10bf28143c35558
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-05-21 15:31:47 +0200

    fix(env): just in case $_ENV does not work

--- config/database.php
+++ config/database.php
@@ -1,14 +1,15 @@
 <?php
-
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../'); 
-$dotenv->load();
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
 
-$host = $_ENV["DB_HOST"];
-$dbname = $_ENV["DB_NAME"];
-$user = $_ENV["DB_USER"];
-$password = $_ENV["DB_PASSWORD"];
+$host = $_ENV['DB_HOST'] ?? getenv('DB_HOST');
+$dbname = $_ENV['DB_NAME'] ?? getenv('DB_NAME');
+$user = $_ENV['DB_USER'] ?? getenv('DB_USER');
+$password = $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD');
 
 try {
     $pdo = new PDO(
@@ -19,7 +20,7 @@ try {
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     error_log($e->getMessage());
     echo "Erreur serveur";
     exit;
