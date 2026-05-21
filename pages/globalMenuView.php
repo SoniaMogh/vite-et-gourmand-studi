808a0a609f3f8f5cf4500b5a70bef4c550cb7f89
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-05-21 12:17:55 +0200

    fix(filter): no filter for now because not working

--- pages/globalMenuView.php
+++ pages/globalMenuView.php
@@ -33,74 +33,7 @@
     </div>
   </div>
 
-  <div id="global-view-filter">
-    <div class="container py-5 px-5">
-      <div class="row bg-white card-corner p-4">
-        <div class="form-display col">
-          <label for="theme">Theme</label>
-          <select
-            name="theme"
-            id="theme"
-            placeholder="tout"
-            class="w-100 form-control"
-          >
-            <option value="Classique">Classique</option>
-            <option value="Evénement">Evénement</option>
-            <option value="Noël">Noël</option>
-            <option value="Halloween">Halloween</option>
-            <option value="Gastronomique">Gastronomique</option>
-            <option value="Saison">Saison</option>
-          </select>
-        </div>
-        <div class="form-display col">
-          <label for="regime">Régime</label>
-          <select
-            name="regime"
-            id="regime"
-            placeholder="tout"
-            class="w-100 form-control"
-          >
-            <option value="Classique">Classique</option>
-            <option value="Vegetarien">Végétarien</option>
-            <option value="Vegan">Végan</option>
-            <option value="Sans-fruit-de-mer">Sans fruit de mer</option>
-          </select>
-        </div>
-        <div class="form-display col">
-          <label for="min-price">Prix minimum</label>
-          <input
-            name="min-price"
-            id="min-price"
-            placeholder="Ex: 25"
-            class="w-100 form-control"
-          >
-          </input>
-        </div>
-        <div class="form-display col">
-          <label for="max-price">Prix maximum</label>
-          <input
-            name="max-price"
-            id="max-price"
-            placeholder="Ex: 100"
-            class="w-100 form-control"
-          >
-          </input>
-        </div>
-        <div class="form-display col">
-          <label for="nbr-pers">Nombre de personne</label>
-          <input
-            name="nbr-pers"
-            id="nbr-pers"
-            placeholder="Ex: 2"
-            class="w-100 form-control"
-          >
-          </input>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div id="menus">
+  <div id="menus" class="pt-5">
     <div class="container pb-5 px-5">
       <div class="menu-card-wrapper">
         <?php foreach ($menus as $menu): ?>
