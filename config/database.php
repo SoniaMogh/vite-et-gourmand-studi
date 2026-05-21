d06c943dda8623262651e4ad6bd4fabfef7d12ba
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-05-21 18:14:59 +0200

    fix(end): trying to connect to db

--- config/database.php
+++ config/database.php
@@ -1,20 +1,21 @@
+
 <?php
 $host = getenv('DB_HOST');
+$port = getenv('DB_PORT') ?: 3306;
 $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
 
 try {
     $pdo = new PDO(
-        "mysql:host=$host;dbname=$dbname",
+        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
         $user,
         $password
     );
-
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
     error_log($e->getMessage());
-    echo "Erreur serveur";
+    echo "DB error";
     exit;
 }
