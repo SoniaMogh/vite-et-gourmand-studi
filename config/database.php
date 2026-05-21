f6a6cf6f50eb6c83fa55e21a02f3d3716bb0190c
SoniaMogh <Soniamoghraoui@hotmail.fr>
2026-05-21 16:35:51 +0200

    fix(env): test without vendor

--- config/database.php
+++ config/database.php
@@ -1,14 +1,8 @@
 <?php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../'); 
-$dotenv->load();
-
-$host = $_ENV["DB_HOST"];
-$dbname = $_ENV["DB_NAME"];
-$user = $_ENV["DB_USER"];
-$password = $_ENV["DB_PASSWORD"];
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
 
 try {
     $pdo = new PDO(
@@ -19,7 +13,7 @@ try {
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-} catch(PDOException $e) {
+} catch (PDOException $e) {
     error_log($e->getMessage());
     echo "Erreur serveur";
     exit;
